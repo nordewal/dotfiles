3ffdbb1d6a094d32d5e9d3de748dbb07b921fccd
nordewal <nordewal@gmail.com>
2020-12-29 08:03:58 +0100

    add git aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,6 +7,17 @@ set -gx PATH $PATH ~/bin
 # no greeting
 set fish_greeting ""
 
+# git alias
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.cm commit
+git config --global alias.st status
+git config --global alias.ad add
+git config --global alias.br branch
+git config --global alias.subup "submodule update --init --recursive"
+git config --global alias.subst "submodule status --recursive"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
 # git abbreviations
 abbr -a g 'git'
 abbr -a ga 'git add'
