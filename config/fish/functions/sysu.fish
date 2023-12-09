1d61fe7d06e2ccb73fb73378fb0f6b90644a0a7e
nordewal <nordewal@gmail.com>
2023-12-09 10:19:01 +0545

    update with pacman, only then with yay

--- config/fish/functions/sysu.fish
+++ config/fish/functions/sysu.fish
@@ -1,5 +1,6 @@
 function sysu
   sudo lvs |grep lvroot_snap_ |awk '{print $1}' |xargs -i sudo lvremove -y /dev/vgroot/{} ; and \
   sudo lvcreate -L 50g -s -n lvroot_snap_(date '+%Y%m%d') /dev/vgroot/lvroot ; and \
-  yay -Syu --answerdiff=None
+  sudo pacman -Syu ; and \
+  yay -Su --answerdiff=None
 end
